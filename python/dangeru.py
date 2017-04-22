946017fcc12bbba51afaf2c2eaae3a9a32c24cf1
naomiEve <stapa2000@gmail.com>
2017-04-22 20:25:42 +0200

    added comments

--- python/dangeru.py
+++ python/dangeru.py
@@ -1,12 +1,30 @@
+'''
+dangeru.py - danger/u/ python API wrapper
+@author prefetcher
+'''
+
 import requests
 import json
 
+'''
+index a board
+@param {string} board - the board
+@param {string} limit - how many threads to show
+@returns JSON formatted into a dictionary
+'''
 def index(board, limit):
     fetch = requests.get("https://boards.dangeru.us/api.php?type=index&board=" + board + "&ln=" + limit.__str__())
     fetch = fetch.text.replace('\n', ' ').replace('\r', '')
     json_f = json.loads(fetch)
     return json_f
 
+'''
+display a thread
+@param {string} board - the board
+@param {string} limit - how many threads to show
+@param {string} threadid - the thread id
+@returns JSON formatted into a dictionary
+'''
 def thread(board, limit, threadid):
     if threadid.startswith("http"):
         threadid = threadid.partition("=")[2]
