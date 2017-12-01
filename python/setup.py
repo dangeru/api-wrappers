109b49aac4613f0668e5b5cb242521bf2c50867c
Niles Rogoff <me@niles.xyz>
2017-11-30 17:55:33 -0800

    Fix gitignore

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name='dangeru',
+	version='1.0',
+	description='danger/u/ api wrapper',
+	author='prefetcher, niles',
+	author_email='me@niles.xyz',
+	url='https://dangeru.us',
+	#packages=['dangeru'],
+	py_modules=['dangeru'],
+)
